25a8f6f468f68230ca48d166413062ef928f5f28
Dominique Maez <Domino@Dominiques-MacBook-Pro-2.local>
2019-04-28 21:29:34 -0700

    from class

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var anchor = document.querySelector('a');
+console.log(anchor);
+var allAnchor = document.querySelectorAll('a');
+console.log (allAnchor);
+
+allAnchor[1].textContent = 'This text is set from JS DOM manipulation';
+
+allAnchor[1].target = '_blank';
+allAnchor[1].href = 'https://www.google.com';
+
+var para1 = document.getElementById('para-1');
+console.log(para1);
+
+var para1UsingSelector = document.querySelector('#para-1');
+console.log(para1UsingSelector);
+
+para1.style.color = 'orange';
